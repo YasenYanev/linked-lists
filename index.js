b70cf4cd78a561abc78b907a21fdfbb73a82a7cb
Yasen Yanev <124697579+YasenYanev@users.noreply.github.com>
2023-11-27 22:16:50 +0200

    contains() and find() methods finished.
    I finished both methods and optimized the code.

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ list.append('First Node');
 list.append('Second Node');
 list.append('Third Node');
 list.append('Fourth Node');
+list.append('Fifth Node');
 
-console.log(list);
-
-list.pop();
-
-console.log(list);
+console.log(list.at(4));
+console.log(list.find('Fifth Node'));
--- linked-list.js
+++ linked-list.js
@@ -1,21 +1,41 @@
-/* eslint-disable no-else-return */
 import Node from './node.js';
 
-function traverseList(index, nodeObj, mode) {
-  if (mode === 'getNode' && index === 0) {
-    return nodeObj;
-  } else if (mode === 'pop' && index === 2) {
-    return nodeObj;
-  }
-  // getIndex
-  return traverseList(index - 1, nodeObj.next, mode);
-}
+// Traverse list function mods:
+// a1 = 'at';
+// a2 = 'pop';
+// b1 = 'contains'
+// b2 = 'find';
 
 export default class LinkedList {
   constructor() {
     this.listSize = 0;
     this.headNode = null;
     this.tailNode = null;
+  }
+
+  #traverseList(
+    mode, // Mode
+    index = this.listSize, // Starting index
+    compareValue = undefined, // Value to compare to(if any)
+    nodeObj = this.headNode // Current node
+  ) {
+    if ((mode === 'a1' && index === 0) || (mode === 'a2' && index === 2)) {
+      return nodeObj;
+    }
+
+    if (mode === 'b1' || mode === 'b2') {
+      if (nodeObj.value !== compareValue && index <= 0) {
+        return null;
+      }
+      if (nodeObj.value === compareValue) {
+        return {
+          b1: true,
+          b2: this.listSize - index,
+        };
+      }
+    }
+
+    return this.#traverseList(mode, index - 1, compareValue, nodeObj.next);
   }
 
   append(value) {
@@ -43,12 +63,19 @@ export default class LinkedList {
     if (index < 0 || index >= this.listSize) {
       return null;
     }
-    return traverseList(index, this.headNode, 'getNode');
+    return this.#traverseList('a1', index);
+  }
+
+  contains(value) {
+    return this.#traverseList('b1', undefined, value).b1;
+  }
+
+  find(value) {
+    return this.#traverseList('b2', undefined, value).b2;
   }
 
   pop() {
-    const penultimateNode = traverseList(this.listSize, this.headNode, 'pop');
-    console.log(penultimateNode);
+    const penultimateNode = this.#traverseList('a2');
     penultimateNode.next = null;
     this.tailNode = penultimateNode;
 
